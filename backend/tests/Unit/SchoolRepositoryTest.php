36e05665e566ae0401c9dcc06177abdf30c7b686
Jason D'Souza <jason.dsouza@bioceuticals.com.au>
2019-07-25 08:45:45 +1000

    Added tests * checked core functionality of retrieving data by mocking classes

--- backend/tests/Unit/SchoolRepositoryTest.php
+++ backend/tests/Unit/SchoolRepositoryTest.php
@@ -2,18 +2,58 @@
 
 namespace Tests\Unit;
 
+use App\Library\Services\SchoolDataServiceInterface;
+use App\Repositories\SchoolRepository;
+use App\Repositories\SchoolStatsRepositoryInterface;
+use Illuminate\Cache\CacheManager;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class SchoolRepositoryTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    class SchoolRepositoryTest extends TestCase
     {
-        $this->assertTrue(true);
+        /**
+         *
+         *
+         * @return void
+         */
+        public function testReturnsDataFromServiceIfCacheisEmpty()
+        {
+            $schoolDataService = $this->mock(SchoolDataServiceInterface::class, function ($mock) {
+                $mock->shouldReceive('fetchData')->once()->andReturn(array());
+            });
+
+            $schoolStats = $this->mock(SchoolStatsRepositoryInterface::class, function ($mock) {
+                $mock->shouldReceive('storeStats')->once()->andReturn(true);
+            });
+
+            $schoolRepository = new SchoolRepository($schoolDataService, $schoolStats, new CacheManager(app()), 60);
+
+            $data = $schoolRepository->getData();
+
+            $this->assertEquals(array(), $data);
+        }
+
+        public function testReturnsDataFromCacheIfCacheisNotEmpty()
+        {
+            $schoolDataService = $this->mock(SchoolDataServiceInterface::class, function ($mock) {
+                $mock->shouldReceive('fetchData')->andReturn(array('some_data'));
+            });
+
+            $schoolStats = $this->mock(SchoolStatsRepositoryInterface::class, function ($mock) {
+                $mock->shouldReceive('storeStats')->andReturn(true);
+            });
+
+            $cacheManager = $this->mock(CacheManager::class, function ($mock){
+                $mock->shouldAllowMockingProtectedMethods();
+                $mock->shouldReceive('has')->once()->andReturn(true);
+                $mock->shouldReceive('get')->andReturn(array('cached_data'));
+            });
+
+            $schoolRepository = new SchoolRepository($schoolDataService, $schoolStats, $cacheManager, 60);
+
+            $data = $schoolRepository->getData();
+
+            $this->assertEquals(array('cached_data'), $data);
+        }
+
     }
-}
